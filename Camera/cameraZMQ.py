5a87f2e6d05d083d393875aac77151c2316c5419
Jorge Chang <106640885+jorgechang@users.noreply.github.com>
2022-11-07 17:21:07 -0500

    Main working with subprocess, ZMQ, and YoloV5.

---
+++ Camera/cameraZMQ.py
@@ -0,0 +1,73 @@
+"""Webcam class."""
+
+# Standar modules
+import socket
+from threading import Thread
+
+# Third party modules
+import cv2
+import imagezmq
+
+
+class WebcamStream:
+    """OCR Model.
+
+    Attributes:
+        queue (faster_fifo.Queue):
+            Webcam frames queue.
+    """
+
+    def __init__(self, queue, stream_id=0):
+        """Init function."""
+        # Accept connections on all tcp addresses, port 5555
+        self.sender = imagezmq.ImageSender(connect_to='tcp://127.0.0.1:5557', REQ_REP=False)
+        # Accept connections on all tcp addresses, port 5555
+        self.sender_processed = imagezmq.ImageSender(connect_to='tcp://127.0.0.1:5558', REQ_REP=False)
+        self.rpi_name = socket.gethostname() # send RPi hostname with each image
+        self.stream_id = stream_id  # default is 0 for main camera
+        self.queue = queue
+        # opening video capture stream
+        self.vcap = cv2.VideoCapture(self.stream_id)
+        if self.vcap.isOpened() is False:
+            print("[Exiting]: Error accessing webcam stream.")
+            exit(0)
+        fps_input_stream = int(self.vcap.get(5))  # hardware fps
+        print(f"FPS of input stream: {fps_input_stream}")
+
+        # reading a single frame from vcap stream for initializing
+        self.grabbed, self.frame = self.vcap.read()
+        if self.grabbed is False:
+            print("[Exiting] No more frames to read")
+            exit(0)
+        # self.stopped is initialized to False
+        self.stopped = True
+        # thread instantiation
+        self.t = Thread(target=self.update, args=())
+        self.t.daemon = True  # daemon threads run in background
+
+    def start(self):
+        """Start thread."""
+        self.stopped = False
+        self.t.start()
+
+    def update(self):
+        """Read next available frame."""
+
+        while self.stopped is not True:
+            self.grabbed, self.frame = self.vcap.read()
+            if self.grabbed is False:
+                print("[Exiting] No more frames to read")
+                self.stopped = True
+                break
+            self.sender.send_image(self.rpi_name, self.frame)
+            self.sender_processed.send_image("processed", self.frame)
+
+        self.vcap.release()
+
+    def read(self):
+        """Return lastest read frame."""
+        return self.frame
+
+    def stop(self):
+        """Stop reading frames."""
+        self.stopped = True
---
+++ Modes/locate.py
@@ -0,0 +1,56 @@
+"""Grasping module with Yolov5."""
+
+# Third party modules
+import cv2
+import imagezmq
+import torch
+
+
+if __name__ == "__main__":
+    # Load Yolov5 model.
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5n')  # or yolov5n - yolov5x6, custom
+    # Box color.
+    bgr = (0, 255, 0)  # color of the box
+    # Get labels.
+    classes = model.names
+    # Font config for the label.
+    label_font = cv2.FONT_HERSHEY_COMPLEX
+
+    # Initialize frame receiver.
+    imagehub = imagezmq.ImageHub(open_port='tcp://127.0.0.1:5557', REQ_REP=False)
+    
+    # Grab new frame.
+    host_name, frame = imagehub.recv_image()
+
+    # Get frame shape.
+    x_shape, y_shape = frame.shape[1], frame.shape[0]
+
+    while True :
+        # Grab new frame.
+        host_name, frame = imagehub.recv_image()
+        # Run object detection inference over frame.
+        results = model(frame)
+        
+        # Get labels and bounding boxes coordinates.
+        labels = results.xyxyn[0][:, -1].cpu().numpy()
+        cord = results.xyxyn[0][:, :-1].cpu().numpy()
+
+        for i in range(len(labels)):
+            row = cord[i]
+            # If confidence score is less than 0.45 we avoid making a prediction.
+            if row[4] < 0.45:
+                continue
+
+            x1 = int(row[0] * x_shape)
+            y1 = int(row[1] * y_shape)
+            x2 = int(row[2] * x_shape)
+            y2 = int(row[3] * y_shape)
+            
+            # Plot the boxes and text.
+            cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)  
+            cv2.putText(frame, classes[int(labels[i])], (x1, y1), label_font, 2, bgr, 2)
+        
+        # Show processed frame.
+        cv2.imshow("processed frame", frame)
+        if cv2.waitKey(1) == ord("q"):
+            pass
