ec34bc3be44d4711fc1561efa7a51a94b4153ae5
Jorge Chang <106640885+jorgechang@users.noreply.github.com>
2023-07-15 14:45:29 -0400

    Update run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -17,5 +17,8 @@ sudo docker run \
        -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
        -v /tmp/.X11-unix/:/tmp/.X11-unix \
        -v /home/paradisolab/Visual_prosthesis:/home \
+       -v /dev:/dev \
+       -v /dev/bus/usb:/dev/bus/usb \
+       --device-cgroup-rule='c 189:* rmw' \
        $MOUNTS \
        vlab 
