7bdb007e0b9ce8e6e4c89396c2cc16a5a9ca0a64
Jorge Chang <106640885+jorgechang@users.noreply.github.com>
2023-07-21 12:19:39 -0400

    adds debug.sh

---
+++ Docker/debug.sh
@@ -0,0 +1,23 @@
+MOUNTS="\
+--device /dev/snd \
+--device /dev/bus/usb \
+--device /dev/video0:/dev/video0:mrw \
+--volume /etc/timezone:/etc/timezone:ro \
+--volume /etc/localtime:/etc/localtime:ro"
+
+sudo systemctl restart docker
+sudo xhost +si:localuser:root
+
+sudo docker run \
+       -it \
+       --rm \
+       --privileged \
+       --runtime nvidia \
+       --network host \
+       -e DISPLAY=$DISPLAY \
+       -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
+       -v /tmp/.X11-unix/:/tmp/.X11-unix \
+       -v /home/paradisolab/full/Visual_prosthesis:/home \
+       --device-cgroup-rule='c 189:* rmw' \
+       $MOUNTS \
+       vlab
