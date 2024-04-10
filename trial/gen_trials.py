dbb120e9184c8d338f2f2e89ca0f0fa1e7b240a1
chang-1 <jorge.chang@lytica.ai>
2024-04-10 10:45:59 -0400

    chore: prettier trials

--- trial/gen_trials.py
+++ trial/gen_trials.py
@@ -12,93 +12,108 @@ def gen_distractor_pos(target_pos, positions, far=True):
 
     #return random.sample(positions[:target_pos] + positions[target_pos+1:], 2)
 
-far = False
+far = True
+number_of_trials = 5
 
-# Read the original CSV file
-df = pd.read_csv('labels_objects.csv')
 
-# Filter rows where distance is not "far"
-if not far:
-    df = df[df['distance'] != 'far']
-else:
-    df = df[df['distance'] != 'near']
+for k in range(number_of_trials):
+    # Read the original CSV file
+    df = pd.read_csv('labels_objects.csv')
 
-original_df = df.copy()
-# Extract the labels and objects columns
-objects = df['labels'].tolist()
-#objects = df['object_name'].tolist()
+    # Filter rows where distance is not "far"
+    if not far:
+        df = df[df['distance'] != 'far']
+    else:
+        df = df[df['distance'] != 'near']
 
-# Create lists for target and distractor positions
-positions = list(range(1, 8))
+    original_df = df.copy()
+    # Extract the labels and objects columns
+    objects = df['labels'].tolist()
+    #objects = df['object_name'].tolist()
 
-# Shuffle the objects to ensure randomness
-random.shuffle(objects)
+    # Create lists for target and distractor positions
+    positions = list(range(1, 8))
 
-# Create a DataFrame for the new CSV
-new_data = []
+    # Shuffle the objects to ensure randomness
+    random.shuffle(objects)
 
-# Fill the first 7 rows with target and distractor positions
-for i in range(7):
-    target_position = i+1
-    target_object = random.choice(objects)
+    # Create a DataFrame for the new CSV
+    new_data = []
 
-    aux_list = []
-    for item in objects[:]:  # Iterate over a copy of the list
-        # Check if the string starts with the given letter
-        if item.startswith(target_object[0]):
-            # Remove the string from the original list and append it to the new list
-            objects.remove(item)
-            aux_list.append(item)
-    
-    distractor_objects = random.sample(objects, 2)
-    distractor_positions = gen_distractor_pos(i+1, positions, far)
-    print(distractor_positions,target_position)
-    row = {
-        'target position': target_position,
-        'target object': f"{target_object} ({KEY[target_object[0]]})",
-        'distractor object position 1': distractor_positions[0],
-        'distractor object 1': f"{distractor_objects[0]} ({KEY[distractor_objects[0][0]]})",
-        'distractor object position 2': distractor_positions[1],
-        'distractor object 2': f"{distractor_objects[1]} ({KEY[distractor_objects[1][0]]})",
-    }
-    new_data.append(row)
-    objects+=aux_list
-    print(len(objects))
+    # Fill the first 7 rows with target and distractor positions
+    for i in range(7):
+        target_position = i+1
+        target_object = random.choice(objects)
 
-# Fill the last 3 rows with random target positions and other columns filled similarly
-for _ in range(3):
-    target_position = random.choice(positions)
-    target_object = random.choice(objects)
+        aux_list = []
+        for item in objects[:]:  # Iterate over a copy of the list
+            # Check if the string starts with the given letter
+            if item.startswith(target_object[0]):
+                # Remove the string from the original list and append it to the new list
+                objects.remove(item)
+                aux_list.append(item)
+        
+        distractor_objects = random.sample(objects, 2)
+        distractor_positions = gen_distractor_pos(i+1, positions, far)
+        print(distractor_positions,target_position)
+        row = {
+            'target position': target_position,
+            'target object': f"{target_object} ({KEY[target_object[0]]})",
+            'distractor object position 1': distractor_positions[0],
+            'distractor object 1': f"{distractor_objects[0]} ({KEY[distractor_objects[0][0]]})",
+            'distractor object position 2': distractor_positions[1],
+            'distractor object 2': f"{distractor_objects[1]} ({KEY[distractor_objects[1][0]]})",
+        }
+        new_data.append(row)
+        objects+=aux_list
+        print(len(objects))
 
-    aux_list = []
-    for item in objects[:]:  # Iterate over a copy of the list
-        # Check if the string starts with the given letter
-        if item.startswith(target_object[0]):
-            # Remove the string from the original list and append it to the new list
-            objects.remove(item)
-            aux_list.append(item)
+    # Fill the last 3 rows with random target positions and other columns filled similarly
+    for _ in range(3):
+        target_position = random.choice(positions)
+        target_object = random.choice(objects)
 
-    distractor_objects = random.sample(objects, 2)
-    distractor_positions = gen_distractor_pos(target_position, positions, far)
-    print(distractor_positions,target_position)
-    row = {
-        'target position': target_position,
-        'target object': f"{target_object} ({KEY[target_object[0]]})",
-        'distractor object position 1': distractor_positions[0],
-        'distractor object 1': f"{distractor_objects[0]} ({KEY[distractor_objects[0][0]]})",
-        'distractor object position 2': distractor_positions[1],
-        'distractor object 2': f"{distractor_objects[1]} ({KEY[distractor_objects[1][0]]})",
-    }
-    new_data.append(row)
-    objects+=aux_list
-    print(len(objects))
+        aux_list = []
+        for item in objects[:]:  # Iterate over a copy of the list
+            # Check if the string starts with the given letter
+            if item.startswith(target_object[0]):
+                # Remove the string from the original list and append it to the new list
+                objects.remove(item)
+                aux_list.append(item)
 
-random.shuffle(new_data)
-# Create DataFrame from the new data
-new_df = pd.DataFrame(new_data)
+        distractor_objects = random.sample(objects, 2)
+        distractor_positions = gen_distractor_pos(target_position, positions, far)
+        print(distractor_positions,target_position)
+        row = {
+            'target position': target_position,
+            'target object': f"{target_object} ({KEY[target_object[0]]})",
+            'distractor object position 1': distractor_positions[0],
+            'distractor object 1': f"{distractor_objects[0]} ({KEY[distractor_objects[0][0]]})",
+            'distractor object position 2': distractor_positions[1],
+            'distractor object 2': f"{distractor_objects[1]} ({KEY[distractor_objects[1][0]]})",
+        }
+        new_data.append(row)
+        objects+=aux_list
+        print(len(objects))
 
-if far:
-    # Save the new DataFrame to a CSV file
-    new_df.to_csv('far_trials_file.csv', index=False)
-else:
-    new_df.to_csv('close_trials_file.csv', index=False)
+    random.shuffle(new_data)
+    # Create DataFrame from the new data
+    new_df = pd.DataFrame(new_data)
+
+    if far:
+        # Save the new DataFrame to a CSV file
+        new_df.to_csv(f'far_trials_{k}_experimental.csv', index=False)
+        random.shuffle(new_data)
+        # Create DataFrame from the new data
+        new_df = pd.DataFrame(new_data)
+        new_df.to_csv(f'far_trials_{k}_control.csv', index=False)
+
+    else:
+        # Save the new DataFrame to a CSV file
+        new_df.to_csv(f'close_trials_{k}_experimental.csv', index=False)
+        random.shuffle(new_data)
+        # Create DataFrame from the new data
+        new_df = pd.DataFrame(new_data)
+        new_df.to_csv(f'close_trials_{k}_control.csv', index=False)
+
+
